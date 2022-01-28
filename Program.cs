51fa8293b74b49bc752715705c543d655d272cc9
Fahim Farhan Khan <fahimfarhan2600@gmail.com>
2022-01-28 00:05:33 -0500

    Commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-
+//Fahim Farhan Khan
 namespace Tech_Assignment2___Basic_Console_App
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Life is too short to remove USB safely");
+
+			Console.WriteLine("Submitted by Fahim Khan");
 		}
 	}
 }
